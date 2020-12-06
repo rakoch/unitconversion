3a93d00a683d5e1060e5537bb43d71f12c55783e
rich <blackrak@gmail.com>
2020-12-06 07:47:17 -0500

    first commit - prjoect setup

---
+++ src/main/java/com/rak/unitconversion/UnitconversionApplication.java
@@ -0,0 +1,13 @@
+package com.rak.unitconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitconversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitconversionApplication.class, args);
+	}
+
+}
